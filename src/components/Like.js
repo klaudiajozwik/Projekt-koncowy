b0b1c92e4a56ed828891b702721625f1d3922abf
klaudiajozwik <klaudia_jozwik@wp.pl>
2023-04-13 13:37:18 +0200

    z

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,3 +1,36 @@
+// import React, { useContext } from "react";
+// import { BooksContext } from "./BookInfo";
+// import { faHeart } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//
+// const Like = () => {
+//     const { bookList } = useContext(BooksContext);
+//     const likedBooks = bookList.filter((book) => book.heart);
+//
+//     return (
+//         <div className="like__container">
+//             <h2 className={"like__headind"}> Polubione </h2>
+//             <div className={"like"}>
+//             {likedBooks.length > 0 ? (
+//                 <div className="like__list">
+//                     {likedBooks.map((book) => (
+//                         <div key={book.id} className="like__item">
+//                             <img src={book.imageLink} alt={book.title} />
+//                             <div className="like__title">{book.title}</div>
+//                             <FontAwesomeIcon icon={faHeart} color="red" />
+//                         </div>
+//                     ))}
+//                 </div>
+//             ) : (
+//                 <div className="like__empty">Brak polubionych książek</div>
+//             )}
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default Like;
+
 import React, { useContext } from "react";
 import { BooksContext } from "./BookInfo";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -9,25 +42,26 @@ const Like = () => {
 
     return (
         <div className="like__container">
-            <h2 className={"like__headind"}> Polubione </h2>
+            <h2 className={"like__heading"}> Polubione </h2>
             <div className={"like"}>
-            {likedBooks.length > 0 ? (
-                <div className="like__list">
-                    {likedBooks.map((book) => (
-                        <div key={book.id} className="like__item">
-                            <img src={book.imageLink} alt={book.title} />
-                            <div className="like__title">{book.title}</div>
-                            <FontAwesomeIcon icon={faHeart} color="red" />
+                {likedBooks.length > 0 ? (
+                    <div className="like__list-container">
+                        <div className="like__list">
+                            {likedBooks.map((book) => (
+                                <div key={book.id} className="like__item">
+                                    <img src={book.imageLink} alt={book.title} />
+                                    <div className="like__title">{book.title}</div>
+                                    <FontAwesomeIcon icon={faHeart} color="red" />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            ) : (
-                <div className="like__empty">Brak polubionych książek</div>
-            )}
+                    </div>
+                ) : (
+                    <div className="like__empty">Brak polubionych książek</div>
+                )}
             </div>
         </div>
     );
 };
 
 export default Like;
-
